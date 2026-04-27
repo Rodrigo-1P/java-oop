197c190e5e193ecfcbf54520b5e8a506c204543a
Rodrigo-1P <a14837@oficina.pt>
2026-04-27 09:50:42 +0000

    criação de atributos para um objeto(viatura)

---
+++ gestao_equipas/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Viatura carro = new Viatura();
+
+        carro.setNumRodas(4);
+        carro.setTipo("Carro Desportivo");
+        carro.setNumRodas(120);
+
+        carro.mostraInfo();
+
+    }
+}
